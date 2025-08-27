4648b716ffcab2da2b0fe91689716b9e1175c0ba
nerudoni <haambayinamasiku@gmail.com>
2025-08-27 08:18:21 +0200

    redirect from dashboard when not logged in

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function dashboard() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/check-login", { withCredentials: true })
+      .then((res) => {
+        if (!res.data.loggedIn) {
+          navigate("/login");
+        }
+      });
+  }, []);
+
   return <h1>Hi dashboard</h1>;
 }
 export default dashboard;
